11a6d8ace80dcdae4a3bb1e8020ce613be002b9e
snow <joaopaulosis@hotmail.com>
2022-07-28 11:02:51 -0300

    order test implementation

--- src/infrastructure/repository/order.repository.spec.ts
+++ src/infrastructure/repository/order.repository.spec.ts
@@ -82,4 +82,72 @@ describe("Order repository test", () => {
             ],
         });
     });
+
+    it("should update a order", async () => {
+        const customerRepository = new CustomerRepository();
+        const customer = new Customer("123", "Customer 1");
+        const address = new Address("Street 1", 1, "Zipcode 1", "City 1");
+        customer.changeAddress(address);
+        await customerRepository.create(customer);
+
+        const productRepository = new ProductRepository();
+        const product = new Product("123", "Product 1", 10);
+        await productRepository.create(product);
+
+        const ordemItem = new OrderItem(
+            "1",
+            product.name,
+            product.price,
+            product.id,
+            2
+        );
+
+        const order = new Order("123", "123", [ordemItem]);
+
+        const orderRepository = new OrderRepository();
+        await orderRepository.create(order);
+
+        const product2 = new Product("1234", "Product 2", 15);
+        await productRepository.create(product2);
+
+        const ordemItem2 = new OrderItem(
+            "2",
+            product2.name,
+            product2.price,
+            product2.id,
+            1
+        );
+
+        order.addItem(ordemItem2);
+        await orderRepository.update(order);
+
+        const orderModel = await OrderModel.findOne({
+            where: { id: order.id },
+            include: ["items"],
+        });
+
+        expect(orderModel.toJSON()).toStrictEqual({
+            id: "123",
+            customer_id: "123",
+            total: order.total(),
+            items: [
+                {
+                    id: ordemItem.id,
+                    name: ordemItem.name,
+                    price: ordemItem.price,
+                    quantity: ordemItem.quantity,
+                    order_id: "123",
+                    product_id: "123",
+                },
+                {
+                    id: ordemItem2.id,
+                    name: ordemItem2.name,
+                    price: ordemItem2.price,
+                    quantity: ordemItem2.quantity,
+                    order_id: "123",
+                    product_id: "1234",
+                },
+            ],
+        });
+    });
 });
--- src/infrastructure/repository/order.repository.ts
+++ src/infrastructure/repository/order.repository.ts
@@ -1,8 +1,9 @@
 import Order from "../../domain/entity/order";
+import OrderRepositoryInterface from "../../domain/repository/order-repository.interface";
 import OrderItemModel from "../db/sequelize/model/order-item.model";
 import OrderModel from "../db/sequelize/model/order.model";
 
-export default class OrderRepository {
+export default class OrderRepository implements OrderRepositoryInterface {
     async create(entity: Order): Promise<void> {
         await OrderModel.create(
             {
@@ -22,4 +23,38 @@ export default class OrderRepository {
             }
         );
     }
+
+    async update(entity: Order): Promise<void> {
+        try {
+            await OrderModel.update(
+                {
+                    customer_id: entity.customerId,
+                    total: entity.total(),
+                    items: entity.items.map((item) => ({
+                        id: item.id,
+                        name: item.name,
+                        price: item.price,
+                        product_id: item.productId,
+                        quantity: item.quantity,
+                    })),
+                },
+                {
+                    where: {
+                        id: entity.id,
+                    },
+                },
+            );
+        } catch (error) {
+            throw new Error("Order not found");
+        }
+    }
+
+    find(id: string): Promise<Order> {
+        throw new Error("Method not implemented.");
+    }
+    findAll(id: string): Promise<Order[]> {
+        throw new Error("Method not implemented.");
+    }
+
+
 }
