5b9f5344e08b8ede506b6cd7fa5142f59cbb6422
snow <joaopaulosis@hotmail.com>
2022-08-01 10:52:26 -0300

    event customer handler when created test

--- src/domain/event/@shared/event-dispatcher.spec.ts
+++ src/domain/event/@shared/event-dispatcher.spec.ts
@@ -1,6 +1,9 @@
+import SendLog2WhenCustomerIsCreatedHandler from "../customer/handler/send-log2-when-customer-is-created.handler";
+import SendLog1WhenCustomerIsCreatedHandler from "../customer/handler/send-log1-when-customer-is-created.handler";
 import SendEmailWhenProductIsCreatedHandler from "../product/handler/send-email-when-product-is-created.handler";
 import ProductCreatedEvent from "../product/product-created.event";
 import EventDispatcher from "./event-dispatcher";
+import CustomerCreatedEvent from "../customer/customer-created.event";
 
 describe("Domain events tests", () => {
   it("should register an event handler", () => {
@@ -79,4 +82,102 @@ describe("Domain events tests", () => {
 
     expect(spyEventHandler).toHaveBeenCalled();
   });
+
+  it("should register an event customer handlers", () => {
+    const eventDispatcher = new EventDispatcher();
+    const eventHandler1 = new SendLog1WhenCustomerIsCreatedHandler();
+    const eventHandler2 = new SendLog2WhenCustomerIsCreatedHandler();
+
+    eventDispatcher.register("CustomerCreatedEvent", eventHandler1);
+    eventDispatcher.register("CustomerCreatedEvent", eventHandler2);
+
+    expect(
+      eventDispatcher.getEventHandlers["CustomerCreatedEvent"]
+    ).toBeDefined();
+    expect(eventDispatcher.getEventHandlers["CustomerCreatedEvent"].length).toBe(
+      2
+    );
+    expect(
+      eventDispatcher.getEventHandlers["CustomerCreatedEvent"][0]
+    ).toMatchObject(eventHandler1);
+    expect(
+      eventDispatcher.getEventHandlers["CustomerCreatedEvent"][1]
+    ).toMatchObject(eventHandler2);
+  });
+
+  it("should unregister an event customer handler", () => {
+    const eventDispatcher = new EventDispatcher();
+    const eventHandler1 = new SendLog1WhenCustomerIsCreatedHandler();
+    const eventHandler2 = new SendLog2WhenCustomerIsCreatedHandler();
+
+    eventDispatcher.register("CustomerCreatedEvent", eventHandler1);
+    eventDispatcher.register("CustomerCreatedEvent", eventHandler2);
+
+    expect(
+      eventDispatcher.getEventHandlers["CustomerCreatedEvent"][0]
+    ).toMatchObject(eventHandler1);
+    expect(
+      eventDispatcher.getEventHandlers["CustomerCreatedEvent"][1]
+    ).toMatchObject(eventHandler2);
+
+    eventDispatcher.unregister("CustomerCreatedEvent", eventHandler1);
+    eventDispatcher.unregister("CustomerCreatedEvent", eventHandler2);
+
+    expect(
+      eventDispatcher.getEventHandlers["CustomerCreatedEvent"]
+    ).toBeDefined();
+    expect(eventDispatcher.getEventHandlers["CustomerCreatedEvent"].length).toBe(
+      0
+    );
+  });
+
+  it("should unregister all event customer handlers", () => {
+    const eventDispatcher = new EventDispatcher();
+    const eventHandler1 = new SendLog1WhenCustomerIsCreatedHandler();
+    const eventHandler2 = new SendLog2WhenCustomerIsCreatedHandler();
+
+    eventDispatcher.register("CustomerCreatedEvent", eventHandler1);
+    eventDispatcher.register("CustomerCreatedEvent", eventHandler2);
+
+    expect(
+      eventDispatcher.getEventHandlers["CustomerCreatedEvent"][0]
+    ).toMatchObject(eventHandler1);
+    expect(
+      eventDispatcher.getEventHandlers["CustomerCreatedEvent"][1]
+    ).toMatchObject(eventHandler2);
+
+    eventDispatcher.unregisterAll();
+
+    expect(
+      eventDispatcher.getEventHandlers["CustomerCreatedEvent"]
+    ).toBeUndefined();
+  });
+
+  it("should notify all event customer handlers", () => {
+    const eventDispatcher = new EventDispatcher();
+    const eventHandler1 = new SendLog1WhenCustomerIsCreatedHandler();
+    const eventHandler2 = new SendLog2WhenCustomerIsCreatedHandler();
+    const spyEventHandler1 = jest.spyOn(eventHandler1, "handle");
+    const spyEventHandler2 = jest.spyOn(eventHandler2, "handle");
+
+    eventDispatcher.register("CustomerCreatedEvent", eventHandler1);
+    eventDispatcher.register("CustomerCreatedEvent", eventHandler2);
+
+    expect(
+      eventDispatcher.getEventHandlers["CustomerCreatedEvent"][0]
+    ).toMatchObject(eventHandler1);
+    expect(
+      eventDispatcher.getEventHandlers["CustomerCreatedEvent"][1]
+    ).toMatchObject(eventHandler2);
+
+    const customerCreatedEvent = new CustomerCreatedEvent({
+      name: "Customer 1",
+    });
+
+    eventDispatcher.notify(customerCreatedEvent);
+
+    expect(spyEventHandler1).toHaveBeenCalled();
+    expect(spyEventHandler2).toHaveBeenCalled();
+  });
+
 });
